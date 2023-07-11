98c01e4e695aadab4b7486f5b366370d16fa9a8d
k.scherbinin <k.scherbinin@digital-core.ru>
2023-07-11 12:37:08 +0500

    ноутбук с обработкой

--- main.py
+++ main.py
@@ -9,21 +9,24 @@ def main(
     img_path: str,
     config_file: str
 ) -> int:
-    mileage = 'NaN'
-    low_threshold = 200
-    while mileage == 'NaN' and low_threshold >= 80:
-        car = CarMileage(img_path, config_file, low_threshold)
-        mileage = car.detect()
-        low_threshold -= 10
-        #print('attempt')
+    low_threshold = 120
+    car = CarMileage(img_path, config_file, low_threshold)
+    mileage = car.detect()
+    if mileage == 'NaN':
+        low_threshold = 170
+        while mileage == 'NaN' and low_threshold >= 80:
+            car = CarMileage(img_path, config_file, low_threshold)
+            mileage = car.detect()
+            low_threshold -= 10
+            #print('attempt')
     return mileage
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--img_path', type=str,
-                        default='data\\original\\autoservice_priem_305_1138747_7.jpg')
+                        default='data\\original\\autoservice_priem_305_1436523_10.jpg')
     parser.add_argument('--config', type=str,
                         default='config\\detection_config.json')
     args = parser.parse_args()
-    main(img_path=args.img_path, config_file=args.config)
+    print(main(img_path=args.img_path, config_file=args.config))
